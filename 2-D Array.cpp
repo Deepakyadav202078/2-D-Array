11e2a0009d6d589e5e92a5f502907b36de8c97bf
Deepakyadav67 <90127239+Deepakyadav67@users.noreply.github.com>
2021-09-25 11:34:35 +0530

    Create 2-D Array.cpp

---
+++ 2-D Array.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+  int a, b, X, count=0;
+
+
+  printf("Enter the number of Row and Column: \n");
+  scanf("%d %d", &a, &b);
+
+
+  int array[a][b];
+
+
+  printf("Enter %d elements: \n", (a*b));
+  for(int i=0; i<a; i++){
+    for(int j=0; j<b; j++){
+      scanf("%d", &array[i][j]);
+    } }
+
+  printf("Enter the element to get the position: \n");
+  scanf("%d", &X);
+
+  for(int i=0; i<a; i++){
+    for(int j=0; j<b; j++){
+      if(array[i][j] == X){
+
+        printf("(%d, %d) \n", i, j);
+        count++;
+      }
+    } }
+
+
+  if(count==0)
+    printf("Not found \n");
+
+  return 0;
+}
