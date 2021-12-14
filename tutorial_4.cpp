75efa01c36b1635148c6435e50f5edb92396a750
Deepak yadav 202078 <90127239+Deepakyadav202078@users.noreply.github.com>
2021-12-14 10:36:26 +0530

    Create tutorial_4.cpp

---
+++ tutorial_4.cpp
@@ -0,0 +1,61 @@
+\\ Call By Value:-
+#include <stdio.h>
+ 
+ 
+void swap(int, int);
+ 
+int main()
+{
+   int x, y;
+ 
+   printf("Enter the value of x and y\n");
+   scanf("%d%d",&x,&y);
+ 
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+ 
+   swap(x, y); 
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", x, y);
+ 
+   return 0;
+}
+ 
+void swap(int a, int b)
+{
+   int temp;
+ 
+   temp = b;
+   b = a;
+   a = temp;
+    printf("Values of a and b is %d  %d\n",a,b);
+}
+
+\\ Call by Reference:-
+#include <stdio.h>
+ 
+void swap(int*, int*);
+ 
+int main()
+{
+   int x, y;
+ 
+   printf("Enter the first and second number: ");
+   scanf("%d%d",&x,&y);
+ 
+   printf("Before Swapping\nFirst = %d\nSecond = %d\n", x, y);
+ 
+   swap(&x, &y); 
+ 
+   printf("After Swapping\nFirst = %d\nSecond = %d\n", x, y);
+ 
+   return 0;
+}
+ 
+void swap(int *a, int *b)
+{
+   int temp;
+ 
+   temp = *b;
+   *b = *a;
+   *a = temp;   
+}
